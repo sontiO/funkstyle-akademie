71c73e25d9af999df782fb14c11a6ef470abb5e5
Jonas Sontheim <js@qlax.de>
2022-02-20 13:34:19 +0100

    added highlight to pricecard, added placeholder footer

--- src/components/PriceCard.svelte
+++ src/components/PriceCard.svelte
@@ -3,11 +3,27 @@
         export let title;
         export let price;
         export let subTitle = "";
+        export let  variant = "primary"
+        export let highlightText = ""
       </script>
-      <div class="w-full items-center justify-center flex ">
-      <div class="relative w-10/12 bg-zinc-700 shadow rounded-lg overflow-hidden sm:p-6 place-self-center  flex flex-row items-center  space-x-32 justify-items-start">
-         <dd class=" text-3xl font-semibold text-gray-200">{title}</dd>
-         <dd class=" text-3xl  text-gray-200">{price} €</dd>
-         <dt class="text-sm font-medium text-gray-200 absolute right-0 px-8">{subTitle}</dt>
-       </div>
-    </div>
+      {#if variant === 'primary'}
+  <div class="w-full items-center justify-center flex ">
+           <div class="relative w-10/12 bg-zinc-700 shadow rounded-lg overflow-hidden sm:p-6 place-self-center  flex flex-row items-center  space-x-32 justify-items-start">
+              <dd class=" text-3xl font-semibold text-gray-100">{title}</dd>
+              <dd class=" text-3xl  text-gray-100">{price} €</dd>
+              <dt class="text-sm font-medium text-gray-200 absolute right-0 px-8">{subTitle}</dt>
+            </div>
+         </div>
+         {:else if variant === 'secondary'}
+         <div class="w-full items-center justify-center flex">
+            <div class="overflow-visible relative w-10/12 bg-zinc-700 shadow rounded-lg sm:p-6 place-self-center  flex flex-row items-center  space-x-32 justify-items-start">
+               <dd class=" text-3xl font-semibold text-gray-100">{title}</dd>
+               <dd class=" text-3xl  text-gray-100">{price} €</dd>
+               <dd class=" text-sm  text-gray-100 absolute right-8">{subTitle}</dd>
+               <div class="rounded-md p-1 px-2 absolute -right-2 -top-4 bg-green-600 text-gray-100 text-sm font-medium ">{highlightText}</div>
+
+             </div>
+          </div>
+      {/if}
+
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@ import ContactForm from "../components/ContactForm.svelte";
 import Team from "../components/Team.svelte";
 import Location from "../components/Location.svelte";
 import Prices from "../components/Prices.svelte";
+import Footer from "../components/Footer.svelte";
 import {getContext} from 'svelte';
 const prismicData = getContext('prismicData');
 console.log(prismicData)
@@ -13,16 +14,14 @@ console.log(prismicData)
 <div class="w-screen h-full bg-zinc-800 overflow-x-hidden">
   <nav class="w-full h-20 flex flex-row justify-between bg-zinc-900/30 text-gray-200 p-6 items-center">
     <h3>funkstyle akademie</h3>
-    <ul class="flex flex-row space-x-6 ">
-      <li><a href="/#stundenplan">Stundenplan</a></li>
+    <ul class="flex flex-row space-x-6 mx-6">
+      <li ><a href="/#stundenplan">Stundenplan</a></li>
       <li><a href="#preise">Preise</a></li>
       <li><a href="#location">Location</a></li>
       <li> <a href="#team">Team</a></li>
       <li><a href="#kontakt">Kontakt</a></li>
     </ul>
   </nav>
-
-  
 
 
   <body class="items-center flex justify-center  flex-col space-y-12 overflow-x-hidden mx-6"> 
@@ -34,5 +33,6 @@ console.log(prismicData)
     <Team />
     <ContactForm />
   </body>
+  <Footer />
   </div>
 
