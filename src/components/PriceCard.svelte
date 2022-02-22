dff50442eb00e67672fded1fef6d3bf148e7ed03
Jonas Sontheim <js@qlax.de>
2022-02-22 21:49:33 +0100

    fixed pricecards mobile, reduce ipsum length

--- src/components/PriceCard.svelte
+++ src/components/PriceCard.svelte
@@ -7,23 +7,21 @@
         export let highlightText = ""
       </script>
       {#if variant === 'primary'}
-  <div class="w-full items-center justify-center flex ">
-           <div class="relative w-10/12 bg-zinc-700 shadow rounded-lg overflow-hidden p-4 sm:p-6 place-self-center  flex flex-row items-center  space-x-12 sm:space-x-32 justify-items-start">
-              <dd class="text-xl sm:text-3xl font-semibold text-gray-100">{title}</dd>
-              <dd class="text-xl sm:text-3xl  text-gray-100">{price} €</dd>
-              <dt class="text-sm font-medium text-gray-200 absolute right-0 px-8">{subTitle}</dt>
-            </div>
+         <div class="w-10/12 text-gray-100 text-xl sm:text-3xl font-semibold p-4 sm:p-6 items-center justify-center flex bg-zinc-700 rounded-lg flex-col">
+           <div class="  flex flex-row items-center justify-between  w-full">
+               <dd>{title}</dd>
+               <dd>{price}€</dd>
+           </div>
+           <dd class="text-sm text-gray-100 w-full">{subTitle}</dd>
          </div>
          {:else if variant === 'secondary'}
-         <div class="w-full items-center justify-center flex">
-            <div class="overflow-visible relative w-10/12 bg-zinc-700 shadow rounded-lg p-4 sm:p-6 place-self-center  flex flex-row items-center space-x-12 sm:space-x-32 justify-items-start">
-               <dd class="text-xl sm:text-3xl font-semibold text-gray-100">{title}</dd>
-               <dd class="text-xl sm:text-3xl  text-gray-100">{price} €</dd>
-               <dd class="text-sm  text-gray-100 absolute right-2 sm:right-8">{subTitle}</dd>
-               <div class="rounded-md p-1 px-2 absolute -right-2 -top-4 bg-green-600 text-gray-100 text-sm font-medium ">{highlightText}</div>
-
-             </div>
+         <div class="relative w-10/12 text-gray-100 text-xl sm:text-3xl font-semibold p-4 sm:p-6 items-center justify-center flex bg-zinc-700 rounded-lg flex-col">
+            <div class="  flex flex-row items-center justify-between w-full">
+                <dd>{title}</dd>
+                <dd>{price}€</dd>
+            </div>
+            <dd class="text-sm text-gray-100 w-full">{subTitle}</dd>
+            <div class="rounded-md p-1 px-2 absolute -right-2 -top-4 bg-green-600 text-gray-100 text-sm font-medium ">{highlightText}</div>
           </div>
       {/if}
-
 
--- src/components/Team.svelte
+++ src/components/Team.svelte
@@ -4,7 +4,7 @@
       <div class="space-y-12">
         <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none text-gray-200">
           <h3 class="text-3xl font-bold tracking-tight sm:text-4xl">Unsere Trainer</h3>
-          <p class="text-xl text-gray-500">Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Laboris mollit amet minim magna mollit qui velit. Incididunt tempor esse anim enim tempor labore irure fugiat elit ipsum. Est magna fugiat aliqua eu consectetur amet. Dolor dolor dolor irure. Mollit velit exercitation enim deserunt magna. Ullamco in ad aute culpa magna non sit voluptate qui ipsum no</p>
+          <p class="text-xl text-gray-500">Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Et voluptate qui ipsum no</p>
         </div>
         <ul  class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
           <li>
