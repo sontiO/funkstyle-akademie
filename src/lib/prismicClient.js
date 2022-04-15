99d7b3dc7c6392b520e12f8b3c616e32e5f04571
Jonas Sontheim <js@qlax.de>
2022-04-15 18:34:11 +0200

    use vercel env variable for apikey

--- src/lib/prismicClient.js
+++ src/lib/prismicClient.js
@@ -1,8 +1,7 @@
 import * as prismic from "@prismicio/client";
-import * as constants from "../apiKey.js";
 
 const repoName = `prismic-testqw1`; // Fill in your repository name
-const accessToken = constants.apiKey;
+const accessToken = process.env.apiKey;
 const endpoint = prismic.getEndpoint(repoName);
 
 // This defines how you will structure URL paths in your project.
