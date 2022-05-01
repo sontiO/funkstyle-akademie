2610d67356f2e19669d34c42fb6ba8d4e166190d
Jonas Sontheim <js@qlax.de>
2022-05-01 09:43:34 +0200

    use vercel env variable for api key, remove package-lock (using yarn)

--- src/lib/prismicClient.js
+++ src/lib/prismicClient.js
@@ -1,13 +1,8 @@
 import * as prismic from "@prismicio/client";
-import * as constants from "../apiKey.js";
 
 const repoName = `prismic-testqw1`; // Fill in your repository name
-const accessToken = constants.apiKey;
+const accessToken = process.env.apiKey;
 const endpoint = prismic.getEndpoint(repoName);
-// This defines how you will structure URL paths in your project.
-// Update the types to match the Custom Types in your project, and edit
-// the paths to match the folder-based routing in your project.
-//const routes = [{}];
 
 const createClient = (fetch) => {
   const clientOptions = {
